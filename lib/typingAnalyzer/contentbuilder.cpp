47bb4782153c9a7dc7ae4c260520decf956fb4c9
Denis Istomin <denisistomin14015@gmail.com>
2024-05-28 19:50:20 +0300

    add abstaract contentBuilder and inherited moduleContentBuilder asynchModuleContentBuilder, modify contentBuilder

--- lib/typingAnalyzer/contentbuilder.cpp
+++ lib/typingAnalyzer/contentbuilder.cpp
@@ -1,36 +1,113 @@
 #include "contentbuilder.h"
 Q_LOGGING_CATEGORY(LC_CONTENT_BUILDER,"Content Builder")
-ContentBuilder::ContentBuilder(QQmlEngine* e)
-	:_context(new QQmlContext(e,e))
-	,_engine(e)
+AContentBuilder::AContentBuilder(QQmlEngine* e)
+	:_engine(e)
+	,_contentParent(nullptr)
+	,_content(nullptr)
 {
 }
-void ContentBuilder::addContextPointer(const QString& name, QObject* p)
-{
-	_context->setContextProperty(name, p);
-}
-void ContentBuilder::setSource(const QUrl& url)
-{
-	_engine->setBaseUrl(url);
-}
-void ContentBuilder::setEngine(QQmlEngine* engine)
+void AContentBuilder::setEngine(QQmlEngine* engine)
 {
 	_engine = engine;
 }
-void  ContentBuilder::setParent(QObject* parent)
+void AContentBuilder::setStatus(BuildStatus other)
 {
-	_parent = parent;
+	if (_status == other)
+		return;
+	_status = other;
+	emit statusChanged();
 }
-QQuickItem* ContentBuilder::build()
+AContentBuilder::BuildStatus AContentBuilder::status() const
 {
-	QQmlComponent comp(_engine, _source,
+	return _status;
+}
+
+void AContentBuilder::setContentParent(QObject* parent)
+{
+	_contentParent = parent;
+}
+bool ContentBuilder::build()
+{
+	QQmlComponent comp(engine(), _source,
 		QQmlComponent::PreferSynchronous);
-	QObject* obj = comp.create(_context);
+	QObject* obj = comp.create(/*_context*/);
+	obj->setParent(parent());
 	if (comp.isError())
 	{
 		qCritical(LC_CONTENT_BUILDER) << "Error while creating content" << ": " << comp.errors();
-		return nullptr;
+		return false;
 	}
-	else
-		return (QQuickItem*)obj;
+	setContent((QQuickItem*)obj);
+	return true;
+}
+//void ContentBuilder::addContextPointer(const QString& name, QObject* p)
+//{
+//	_context->setContextProperty(name, p);
+//}
+void ContentBuilder::setSource(const QUrl& url)
+{
+	engine()->setBaseUrl(url);
+}
+void ModuleContentBuilder::setModule(const QString& other)
+{
+	_module = other;
+}
+QString ModuleContentBuilder::module() const
+{
+	return _module;
+}
+QString ModuleContentBuilder::name() const
+{
+	return _name;
+}
+void ModuleContentBuilder::setName(const QString& name)
+{
+	_name = name;
+}
+//void ModuleContentBuilder::addContextPointer(const QString& name, QObject* p)
+//{
+//	_context.setContextProperty(name, p);
+//}
+
+bool ModuleContentBuilder::build()
+{
+	QQmlComponent comp(engine());
+	comp.loadFromModule(module(), name());
+	QObject* obj = comp.create(/*&_context*/);
+	obj->setParent(parent());
+	if (comp.isError())
+	{
+		qCritical(LC_CONTENT_BUILDER) << "Error while creating content" << ": " << comp.errors();
+		return false;
+	}
+	setContent((QQuickItem*)obj);
+	return true;
+}
+bool AsynchModuleContentBuilder::build() 
+{
+	QQmlComponent comp(engine());
+	comp.loadFromModule(module(), name());
+	comp.create(*this);
+	return true;
+}
+void AsynchModuleContentBuilder::statusChanged(QQmlIncubator::Status status)
+{
+	switch (status)
+	{
+	case QQmlIncubator::Null:
+		break;
+	case QQmlIncubator::Ready:
+		setContent((QQuickItem*)object());
+		setStatus(Built);
+		break;
+	case QQmlIncubator::Loading:
+		setStatus(notBuilt);
+		break;
+	case QQmlIncubator::Error:
+		setStatus(BuildStatus::Error);
+		qCritical(LC_CONTENT_BUILDER) << "Error while creating content" << ": " << errors();
+		break;
+	default:
+		break;
+	}
 }
--- lib/typingAnalyzer/contentbuilder.h
+++ lib/typingAnalyzer/contentbuilder.h
@@ -1,22 +1,75 @@
 #pragma once
 #include <qobject.h>
 #include <qquickitem.h>
+#include <qqmlincubator.h>
 #include <qqmlengine.h>
 #include <qqmlcontext.h>
 #include <qloggingcategory.h>
 Q_DECLARE_LOGGING_CATEGORY(LC_CONTENT_BUILDER)
-class ContentBuilder
+class AContentBuilder : public QObject
+{
+	Q_OBJECT;
+public:
+	enum BuildStatus
+	{
+		notBuilt,
+		Built,
+		Error
+	}; Q_ENUM(BuildStatus);
+	BuildStatus status() const;
+	explicit AContentBuilder(QQmlEngine* engine);
+	void setContentParent(QObject* parent);
+	void setEngine(QQmlEngine* en);
+	virtual bool build() = 0;
+	virtual QQuickItem* content() const;
+	QQmlEngine* engine() const;
+	QObject* contentParent() const;
+signals:
+	void statusChanged();
+protected:
+	void setStatus(BuildStatus other);
+	virtual ~AContentBuilder() = default;
+	virtual void setContent(QQuickItem* other);
+private:
+	Q_PROPERTY(BuildStatus status READ status NOTIFY statusChanged);
+
+	QObject* _contentParent;
+	QQmlEngine* _engine;
+	QQuickItem* _content;
+	BuildStatus _status;
+};
+class ContentBuilder : public AContentBuilder
 {
 public:
-	explicit ContentBuilder(QQmlEngine* e);
-	void addContextPointer(const QString& name, QObject* p);
+	explicit ContentBuilder(QQmlEngine* e, const QUrl& url);
+//	void addContextPointer(const QString& name, QObject* p);
+	bool build() override;
 	void setSource(const QUrl& url);
-	void setEngine(QQmlEngine* engine);
-	void setParent(QObject* parent);
-	QQuickItem* build();
 private:
-	QQmlContext* _context;
-	QQmlEngine* _engine;
+//	QQmlContext* _context;
 	QUrl _source;
-	QObject* _parent;
+};	
+class ModuleContentBuilder : public AContentBuilder
+{
+public:
+	explicit ModuleContentBuilder(QQmlEngine* e, const QString& name, const QString& module = "TypingAnalyzer");
+//	void addContextPointer(const QString& name, QObject* p);
+	void setModule(const QString& other);
+	QString module() const;
+	QString name() const;
+	void setName(const QString& name);
+	bool build() override;
+private:
+	QString _module;
+	QString _name;
+//	QQmlContext _context;
+
+};
+class AsynchModuleContentBuilder: public ModuleContentBuilder, public QQmlIncubator
+{
+public:
+	using ModuleContentBuilder::ModuleContentBuilder;
+	bool build() override;
+protected:
+	void statusChanged(QQmlIncubator::Status status) override;
 };
